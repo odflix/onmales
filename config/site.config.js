6dafcc1b7bc74cfef428419cdc869ca40574ce78
odflix <54980077+odflix@users.noreply.github.com>
2023-07-12 00:13:49 +0530

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -20,7 +20,7 @@ module.exports = {
   title: "Kolder Creative",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
-  baseDirectory: '/Sike Cources/Kold Creatives',
+  baseDirectory: '/classes/Kold Creatives',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
@@ -45,7 +45,7 @@ module.exports = {
   protectedRoutes: [],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'mailto:spencer.wushangbo@gmail.com',
+  email: 'mailto:hi@mirrorlessfilms.in',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
